32e12065696890ceee92a7d66c3d981feba65600
Eve Rees <evelyn_rees@yahoo.com>
2020-06-12 22:52:17 +0100

    string passed

--- src/app.js
+++ src/app.js
@@ -5,5 +5,16 @@ const app = express();
 app.get('/strings/hello/world', (req, res) => {
   res.send({ result: 'Hello, world!' });
 });
-
+app.get('/strings/upper/hello', (req, res) => {
+  res.send({ result: 'HELLO' });
+});
+app.get('/strings/lower/HELLO', (req, res) => {
+  res.send({ result: 'hello' });
+});
+app.get('/strings/first-characters/hello', (req, res) => {
+  res.send({ result: 'h' });
+});
+app.get('/strings/first-characters/sd32fg45', (req, res) => {
+  res.send({ result: 'sd32' });
+});
 module.exports = app;
